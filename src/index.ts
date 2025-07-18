65b5b27aac987bebc63bf844c2eab8df2ee7475a
Yigal Irani <yigal@symbolclick.com>
2025-07-17 22:49:36 -0400

    kysely starts working, perses config file with zod and types

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import Fastify,{FastifyInstance,} from 'fastify'
 import mysql,{MySQLPool}from '@fastify/mysql'
 import { readFileSync } from 'fs';
+import {DB} from './autogen/database'
 
 import { z ,ZodType } from "zod";
+import { createPool } from 'mysql2' 
+import { Kysely, MysqlDialect } from 'kysely'
 const config_schema = z.object({
   connectionString: z.string(),
+  connection:z.object({
+    database: z.string(),
+    host: z.string(),
+    user: z.string(),
+    password: z.string(),
+    port: z.number(),
+    connectionLimit: z.number()
+  })
 });
 function read_zod<T>(filename: string, schema: ZodType<T>): T {
   const config_data = readFileSync(filename, 'utf-8');  //read sync so doent need the buildfasity pattern
@@ -15,21 +26,31 @@ declare module 'fastify' {
     mysql: MySQLPool
   }
 }
-const {connectionString}=read_zod('./config.json',config_schema)
+async function build_server(app:FastifyInstance){
+  const {connection,connectionString}= read_zod('./config.json',config_schema)
 
-async function  make_app(app:FastifyInstance){
+  const dialect = new MysqlDialect({
+    pool: createPool(connection)
+  })
+
+  const db = new Kysely<DB>({
+    dialect,
+  })
+  const users=await db.selectFrom('mc_post').selectAll().execute()
+  console.log(users)
+
   await app.register(mysql, {
-    connectionString,
-    promise: true,
+    connectionString
   })
   // Declare a route
   app.get('/', async function handler (request, reply) {
-    const [rows] = await app.mysql.query('SELECT * FROM your_table')
+    const rows=await app.mysql.query('SELECT * FROM your_table')
     reply.type('text/html').send('<h1>hello</h1> this is html')
   })
 }
 
 const app = Fastify({logger: true})
+build_server(app)
 app.listen({ port: 3000 })
 
 
